e8b5de990dbd720c30844d605cf76ead9aed7340
JamesTJenkins <jenkins01tj@protonmail.com>
2025-04-24 17:27:55 +0100

    Added AtlantisDefines.h for easier management of tags and keys across the project

---
+++ Source/Atlantis/Public/AtlantisDefines.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// TAGS
+#define PLAYER_TAG FName("Player")
+
+// BLACKBOARD KEYS
+#define PATROL_POINT_KEY FName("PatrolLocation")
+#define ENEMY_KEY FName("Enemy")
+#define HAS_LINE_OF_SIGHT_KEY FName("HasLineOfSight")
